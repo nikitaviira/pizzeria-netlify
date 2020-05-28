e44ad0d8fbecbe09f2eb608987bbacd83703a5e6
Nikita Viira <niviir@ttu.ee>
2020-05-28 15:35:11 +0300

    netlify

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,7 +7,7 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        baseURL: 'https://localhost:5001/api/'
+        baseURL: 'https://pizzeria-nekitosa.azurewebsites.net/api/'
     },
     modules: {
         user: User,
